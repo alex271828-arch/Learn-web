6c7ef49a9e1492833ac618c9a3e3fbdf11e56d99
alex271828-arch <serge4@optonline.net>
2024-05-24 17:29:24 +0400

    start operators js

--- JavaScript/lesson-01/main.js
+++ JavaScript/lesson-01/main.js
@@ -22,3 +22,47 @@ const apples = 10;
 console.log(userName, "has", apples, "apples");
 
 //* Data types (примітивні типи даних)
+/*
+? - Number - це числові дані, тобто це будь-які числа (0, -7, 157, 5.378)
+
+? - String - це текстова інформація, тобто, будь-що записане у лапки ("", '', ``) - "Hello", "50"
+
+? - Boolean(bool) - логічний тип даних, який вміє зберігати тільки два значення - правда і неправда, тобто true or false
+
+? - undefined - відсутність значення(не визначено)
+
+? - null - пустота, нульове значення, відсутність посилання на щось
+*/
+
+let someValue;
+
+console.log(10); // Number
+console.log("10"); // String
+console.log(true); // Boolean
+console.log(someValue); // undefined - значення не було надано, тобто змінна невизначена
+
+//* typeof - це оператор, який повертає тип даних якогось значення
+
+console.log(typeof false); // boolean
+console.log(typeof 10); // number
+console.log(typeof "7"); // string
+
+//! ================================
+
+// +, -, *, /, %, **
+
+const a = 11;
+const b = 5;
+
+console.log(a + b); // 16
+console.log(a - b); // 6
+console.log(a * b); // 55
+console.log(a / b); // 2.2
+console.log(a % b); // 1
+console.log(17 % 3); // 2
+console.log(2 ** 3); // 8
+
+
+console.log(100 + 50 - 2 * 5); // 140
+
+// += - комбіновані оператори
