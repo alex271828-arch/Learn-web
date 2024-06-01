57836c5b67fd82cd30464ce6de97f3f38d96366a
alex271828-arch <serge4@optonline.net>
2024-06-01 15:49:26 +0400

    functions js

--- JavaScript/lesson-01/main.js
+++ JavaScript/lesson-01/main.js
@@ -159,6 +159,94 @@ console.log(true + 1 === 2); // true + 1 = 1 + 1 = 2
 
 console.log(null === undefined); // false
 
-console.log("13" === 1 + "3"); 
+console.log("13" === 1 + "3");
 // 1. 1 + "3" = "13"
 // 2. "13" === "13" - true
+
+console.log(2 > "2"); // false
+
+console.log("5" === 3 + 2); // "5" === 5 -> string === number -> false
+
+console.log(6 === "3" * 2); // 6 === Number("3") * 2 -> 6 === 3 * 2 -> 6 === 6 -> true
+
+console.log(true + 5 === "10" - 4); // 1 + 5 === 10 - 4 -> 6 === 6 -> true
+
+console.log("10" - 4);
+console.log("10" + 4);
+
+// int - 6, 10, -3, 100
+// float - 4.2, 60.85, -190.742
+
+const elementWidth = "50.59px";
+console.log(Number(elementWidth)); // NaN
+console.log(Number.parseInt(elementWidth)); // 50
+console.log(Number.parseFloat(elementWidth)); // 50.59
+
+const value = 27.4;
+
+console.log(Math.floor(value)); // 27
+console.log(Math.ceil(value)); // 28
+console.log(Math.round(value)); // 27
+
+//! === Functions ===
+
+//* DRY - do not repeat yourself
+
+// console.log("Hello world!!!");
+// console.log("Hello world!!!");
+// console.log("Hello world!!!");
+// console.log("Hello world!!!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+
+// * функція - це спеціальна конструкція (контейнер) в якій можна описати деякий функціонал (код) один раз і потім викоритсовувати його багато разів в різних місцях програми. Таким чином алгоримт зберігається один раз в одному місці і в разі вимушених змін коду це стає набагто простіше, тому фукнціями легко керувати.
+
+// logInfo - function name
+// () - parameters list
+// {} - function body
+
+function logInfo() {
+  console.log("== Hello world! ==");
+}
+
+// виклик фукнції (запуск коду функції)
+// () - arguments list
+// logInfo()
+// logInfo()
+// logInfo()
+// logInfo()
+// logInfo()
+// logInfo()
+// logInfo()
+
+// name - це параметр, фактично це локальна змінна яка існує тільки всередині функції і отримує своє значення під час виклику цієї функції
+
+function greeting(name, yearOfBirth) {
+  console.log(`Hello ${name}! Your age is: ${2024 - yearOfBirth} years old.`);
+}
+
+// "alex" - це аргумент який присвоїться твоєму параметру
+greeting("Alex", 2009);
+greeting("Oleg", 1985);
+greeting("Sergey", 1964);
+
+//* напиши функцію calculateRectangle(width, height) - яка приймає висоту і ширину прямокутника. Ця функція повинна вивести в консоль периметр та площу цього прямокутника
+
+function calculateRectangle(width, length) {
+  console.log(`P = ${(width + length) * 2} | S = ${width * length}`);
+}
+
+calculateRectangle(3, 4);
+calculateRectangle(12, 15);
+
+//* напиши функцію add(a, b) - яка приймає два числа і виводить в консоль суму цих чисел
+
+function add(a, b) {
+  console.log(`${a} + ${b} = ${Number(a) + Number(b)}`);
+}
+
+add(502, 287589435697)
+add(2, "5")
+add("10", 4)
