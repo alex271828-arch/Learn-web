af62fbd05ebbdc97d209353b8b345726b5e12d23
alex271828-arch <serge4@optonline.net>
2024-06-10 17:23:59 +0400

    logical operators && boolean coercion

--- JavaScript/module-02/main.js
+++ JavaScript/module-02/main.js
@@ -65,7 +65,6 @@ if (daysUntilDeadline === 0) {
   console.log("Date in the future");
 }
 
-
 //* logical operators && boolean coercion
 
 // console.log(true + 1); // 2 - неявне перетворення типу
@@ -88,7 +87,6 @@ if (daysUntilDeadline === 0) {
 //* logical AND (&&) - повертає перше значення, яке конвертується у false, якщо такого немає - повертає останнє
 //* logical NOT (!) - змінює логічний тип значення на зворотнє
 
-
 //* logical NOT (!)
 
 console.log(!true); // false
@@ -109,3 +107,53 @@ console.log(NaN || "" || "false" || true || -5); // "false"
 // Boolean(NaN) - false
 // Boolean("") - false
 // Boolean("false") - true
+
+console.log(Boolean(0)); // false
+console.log(Boolean(-5.4)); // true
+console.log(Boolean("false")); // true
+console.log(Boolean("0")); // true
+console.log(Boolean("")); // false
+console.log(Boolean(" ")); // true
+console.log(Boolean(null)); // false
+
+console.log("5" + 3); // "5" + String(3) = "5" + "3" = "53" - string
+console.log("5" - 3); // Number("5") - 3 = 5 - 3 = 2 - number
+
+console.log("h" - 2); // Number("h") - 2 = NaN - 2 = NaN
+
+console.log(!false); // true
+console.log(!0); // not false -> true
+console.log(!"hello"); // !true -> false
+console.log(true || false); // true
+console.log(0 || 1 || 5 || false); // 1
+console.log("" || 0 || false || "0" || null); // "0"
+console.log("" || false || null || undefined || 0); // 0
+
+//* logical AND (&&) - returns first false
+
+console.log(true && false); // false
+console.log(5 && true && "0" && 0 && "hello"); // 0
+console.log("hello" && 10 && null && " " && false); // null
+console.log(10 && true && " " && "0" && -7); // -7
+
+const number = 500;
+const rangeStart = 10;
+const rangeEnd = 100;
+
+if (number >= rangeStart && number <= rangeEnd) {
+  console.log(`Number ${number} is in range [${rangeStart}, ${rangeEnd}]`);
+} else {
+  console.log(`Number ${number} isn't in range [${rangeStart}, ${rangeEnd}]`);
+}
+
+// ================
+
+const sub = "pro"; // "free", "pro", "vip"
+const canAccessContent = sub === "pro" || sub === "vip";
+console.log("canAccessContent", canAccessContent);
+
+if (canAccessContent) {
+  console.log("You're welcome");
+} else {
+  console.log("Bye bye");
+}
